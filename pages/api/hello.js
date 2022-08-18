8778071c2981b4af7078a1a3716987223933d088
Dustin <dustinguzman17@gmail.com>
2022-08-18 17:42:26 +0800

    process env test id

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ test_id: process.env.TEST_ID })
 }
